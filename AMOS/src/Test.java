0ad46034cd7469bca0586451b8a8705cce9a2b7a
matthi <Matthi@Matthi-PC>
2013-04-29 21:10:30 +0200

    Test 2

--- AMOS/src/Test.java
+++ AMOS/src/Test.java
@@ -7,5 +7,6 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is a Test");
+		System.out.println("This is a Test AGAIN");
 	}
 }
