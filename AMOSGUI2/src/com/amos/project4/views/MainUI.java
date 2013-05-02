ae845ea577600369d381b26377dd44e482ef51b7
ieigner <isa.eigner@gmx.at>
2013-05-02 11:19:38 +0300

    Update MainUI.java

--- AMOSGUI2/src/com/amos/project4/views/MainUI.java
+++ AMOSGUI2/src/com/amos/project4/views/MainUI.java
@@ -1,8 +1,6 @@
 package com.amos.project4.views;
 /*
- * Copyright (c) 2006-2009 by Dirk Riehle, http://dirkriehle.com
  *
- * This file is part of the Wahlzeit rating application.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
